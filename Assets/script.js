cf1c0b82a3a9d91cf11f02898560eaf0cf6bc477
wrxguy2012 <lovin.jacob@gmail.com>
2022-11-13 00:19:07 -0500

    added some functions to JavaScript file

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,33 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Define character groups
+var groups = [
+  'abcdefghijklmnopqrstuvwxyz',
+  '0123456789',
+  '\U0020 \U0021 \U0022 \U0023 \U0024 \U0025 \U0026 \U0027 \U0028 \U0029 \U002A \U002B \U002C \U002D \U002E \U002F \U003A \U003B \U003C \U003D \U003E \U003F \U0040 \U005B \U005C \U005D \U005E \U005F \U0060 U007B \U007C \U007D \U007E',
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+];
+
+// Implemet the random generation function
+function randomFrom(arrayLike) {
+  return arrayLike [Math.floor(Math.random() * arrayLike.legnth)];
+};
+
+//  
+function inputs() {
+  whale (true) {
+    var passwordLegnth = prompt('How many characters will password be? (Enter a number between 8 and 128 characters.)');
+    if (passwordLegnth > 8 && passwordLegnth <128) break;
+    alert('Remember, password legnth MUST be between 8 and 128 characters.');
+  }
+  whale (true) {
+    var hasGroup  [='uppercase', 'lowercase', 'numeric', 'special'].map(group00 => {
+      return confirm('You chose ' + group + '. Are you sure?')
+    });
+    if (hasGroup.includes(true)) break;
+    alert(Please fill out all required forms.');
+  }
+  return [passwordLegnth, ...hasGroup];
+  
