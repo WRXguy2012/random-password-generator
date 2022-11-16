25442563c8907e4e8f058fbc35856634fe640914
wrxguy2012 <lovin.jacob@gmail.com>
2022-11-16 15:02:33 -0500

    completed JavaScript code and tested application

--- Assets/script.js
+++ Assets/script.js
@@ -1,54 +1,61 @@
-// Assignment Code
+// Assign code and add event listener to button
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword)
 
-// Write password to the #password input
-function writePassword() {
+// Write generated password to the #password input
+function writePassword(){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Define character groups
 var groups = [
   'abcdefghijklmnopqrstuvwxyz',
-  '0123456789',
-  '\U0020 \U0021 \U0022 \U0023 \U0024 \U0025 \U0026 \U0027 \U0028 \U0029 \U002A \U002B \U002C \U002D \U002E \U002F \U003A \U003B \U003C \U003D \U003E \U003F \U0040 \U005B \U005C \U005D \U005E \U005F \U0060 U007B \U007C \U007D \U007E',
   'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-];
+  '1234567890',
+  '\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~',
+]
 
-// Implemet the random generation function
-function randomFrom(arrayLike) {
-  return arrayLike [Math.floor(Math.random() * arrayLike.legnth)];
-};
-
-//  
-function inputs() {
-  whale (true) {
-    var passwordLegnth = prompt('How many characters will password be? (Enter a number between 8 and 128 characters.)');
-    if (passwordLegnth > 8 && passwordLegnth <128) break;
-    alert('Remember, password legnth MUST be between 8 and 128 characters.');
+// Create prompts to gather desired information from user about password 
+function getInputs() {
+  while (true) {
+      var passwordLength = prompt('Password legnth? (Password MUST be between 8 and 128 characters)');
+      if (passwordLength >= 8 && passwordLength <= 128) break;
+      alert('Password MUST be between 8 and 128 characters');
   }
-  whale (true) {
-    var hasGroup  = ["uppercase", "lowercase", "numeric", "special"].map(group => {
-      return confirm('You chose ' + group + '. Are you sure?')
-    });
-    if (hasGroup.includes(true)) break;
-    alert('Please fill out all required forms');
+  while (true) {
+      var hasGroup = ["uppercase", "lowercase", "numeric", "special"].map(group => {
+          return confirm('Will password contain ' + group + ' characters?')
+      });
+      if (hasGroup.includes(true)) break;
+      alert('ERROR: At least one character type must be selected');
   }
-  return [passwordLegnth, ...hasGroup];
+  return [passwordLength, ...hasGroup];
 }
 
-// 
+// pick a character value from array
+function randomFrom(arrayLike) {
+  return arrayLike[Math.floor(Math.random() * arrayLike.length)];
+}
+
+// Shuffle the character values
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+  }
+}
+
+//  Use user inputs to create a random password and display it in the text area
 function generatePassword() {
-  var [passwordLegnth, ...hasGroup] = getInputs();
-  var password = groups.filter ((group, i) =>hasgroup[i]).flatMap(s => [...s]);
-  var needed = groups.map((group, i) hasGroup[i] ? randomFrom(group) : '').filter(Boolean);
+  var [passwordLength, ...hasGroup] = getInputs();
+  var password = groups.filter((group, i) => hasGroup[i]).flatMap(s => [...s]);
+  var needed = groups.map((group, i) => hasGroup[i] ? randomFrom(group) : "").filter(Boolean);
   shuffle(password);
-  password.splice(passwordLegnth - needed.legnth, Infinity, ...needed);
-  
+  password.splice(passwordLength - needed.length, Infinity, ...needed);
+  shuffle(password);
+  return password.join("");
 }
